9720ce09acecd2c06f28a2adaecddd0861760814
Kristine Richard <kristine@kristinerichard.com>
2022-02-15 14:17:24 -0400

    basic blog functionality

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,8 +1,9 @@
 import sqlite3
 
-import click 
+import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+
 
 def get_db():
     if 'db' not in g:
@@ -10,8 +11,10 @@ def get_db():
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-    
+        g.db.row_factory = sqlite3.Row
+
     return g.db
+
 
 def close_db(e=None):
     db = g.pop('db', None)
